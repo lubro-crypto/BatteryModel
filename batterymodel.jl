80d0a120ecd5add9dde5680d2538b60c4dadaa53
lubro-crypto <lucasbrown54321@gmail.com>
2024-08-03 14:44:27 +0100

    Nearly there

--- batterymodel.jl
+++ batterymodel.jl
@@ -35,11 +35,11 @@ optimise_battery_charge(prices, params, del_t=1800)
 - -ve means discharging the battery 
 - Using the apple definition of the number of cycles - 
 Inputs:
-prices: N x 2 matrix representing the two market prices in gbp/MW per half hour
+prices: N x 1 matrix representing the two market prices in gbp/MW per half hour
 params: parameters of the battery charge
 del_t: The time step between each sample - by default 30mins = 1800s
 Outputs: 
-energies_in: N x 2 matrix representing the amount of power going into the battery from market 1 (col 1) and from market 2 (col 2)
+energies_in: N x 1 matrix representing the amount of power going into the battery from market 1 (col 1) and from market 2 (col 2)
 total_profit: float representing the total costs 
 """ 
 function optimise_battery_charge(prices, params::BatteryParams, del_t=1800)
@@ -54,25 +54,37 @@ function optimise_battery_charge(prices, params::BatteryParams, del_t=1800)
     model = Model()
 
     @variable(model, energy_in1[1:N]) # For market 1
-    @variable(model, energy_in2[1:N]) # For market 2
     @variable(model, powers[1:N])
     @variable(model, energies[1:N])
     @variable(model, cycles[1:N])
     @variable(model, maximum_capacities[1:N])
     
+    # Initialisation
     fix(energies[1], 0.0, force=true) # Assume that the battery is not charged at all 
     fix(cycles[1], 0.0, force=true)
-    fix(maximum_capacities[1], params.capacity, force=true)
+    fix(maximum_capacities[:], params.capacity, force=true) # For there is no change in the max capacities 
 
-    @constraint(model, power_con, powers == (energy_in1 + energy_in2)./del_t)
+    # Inequality constraints 
     @constraint(model, power_min_max, -params.max_discharge_rate < powers < params.max_charge_rate)
     @constraint(model, energy_min_max, 0.0 < energies < maximum_capacities)
     @constriant(model, cycles_min_max, 0.0 < cycles < params.max_charges)
     @constraint(model, maximum_capacities_min_max, 0.0 < maximum_capacities < params.capacity)
+    @constraints(model, energy_in_con[i=1:N], - energies[i] <= energy_in1[i] <= maximum_capacities[i] - energies[i]) 
 
-    @constraints(model, energy_con[i=1:N-1], energies[i+1] ==  ) # For now just assume efficieny is the same 
+    # Equality constraints 
+    @constraint(model, power_con, powers == (energy_in1)./del_t)
+    @constraints(model, energy_con[i=1:N-1], energies[i+1] == energy_in1[i]*params.charging_efficiency) # For now just assume charging and discharging gives the same 
+    @constraints(model, cycles_con[i=1:N-1], cycles[i+1] == cycles[i] + abs(energy_in[i])/maximum_capacities[i] )
+
+    # Reduce the cost as much as possible
+    @objective(model, Min, sum(energy_in[i]*prices[i] for i in 1:N))
+
+    optimize!(model)
+    @assert is_solved_and_feasible(model)
+    solution_summary(model)
 
     if overflow > 0.0
         # Add overflow zeros to the end
+
     end
 end
