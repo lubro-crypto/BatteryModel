8c508e17c29755f19581ce1b85ee8122dcdebec2
lubro-crypto <lucasbrown54321@gmail.com>
2024-08-06 23:41:01 +0100

    2 markets are now integrated

--- batterymodel.jl
+++ batterymodel.jl
@@ -38,12 +38,16 @@ optimise_battery_charge(prices, params, del_t=1800)
 - -ve means discharging the battery 
 - Using the apple definition of the number of cycles - 
 Inputs:
-prices: N x 1 matrix representing the two market prices in gbp/MW per half hour
+prices: N x 2 matrix representing the two market prices in gbp/MW per half hour
 params: parameters of the battery charge
 del_t: The time step between each sample - by default 30mins = 1800s
 Outputs: 
-energies_in: N x 1 matrix representing the amount of power going into the battery from market 1 (col 1) and from market 2 (col 2)
-total_profit: float representing the total costs 
+energies_in: N x 2 matrix representing the amount of power going into the battery from market 1 (col 1) and from market 2 (col 2)
+energies_out: N x 2 matrix representing the amount of power going out of the battery to market 1 (col 1) and to market 2 (col 2)
+energy: N x 1 vector representing the battery energy 
+cycles: N x 1 vector representing the battery cycles
+maximum_capacities: N x 1 vector representing the maximum capacity of the battery
+powers: N x 1 vector representing the battery power 
 """ 
 function optimise_battery_charge(prices, params::BatteryParams, del_t=1800)
     @infiltrate
@@ -57,10 +61,12 @@ function optimise_battery_charge(prices, params::BatteryParams, del_t=1800)
     
     model = Model(HiGHS.Optimizer)
     print("Nearly there")
-    @variable(model, energy_in1[1:N], lower_bound=0.0 ) # For market 1
-    @variable(model, energy_out1[1:N], lower_bound = 0.0)
+    @variable(model, energy_in1[1:N], lower_bound=0.0, upper_bound = params.max_storage_volume ) # For market 1
+    @variable(model, energy_out1[1:N], lower_bound = 0.0, upper_bound = params.max_storage_volume)
+    @variable(model, energy_in2[1:N], lower_bound=0.0, upper_bound = params.max_storage_volume) # For market 2
+    @variable(model, energy_out2[1:N], lower_bound = 0.0, upper_bound = params.max_storage_volume)
     @variable(model, powers[1:N], lower_bound=-params.max_discharge_rate, upper_bound=params.max_charge_rate)
-    @variable(model, energies[1:N], lower_bound=0.0)
+    @variable(model, energies[1:N], lower_bound=0.0, upper_bound = params.max_storage_volume)
     @variable(model, cycles[1:N], lower_bound=0.0, upper_bound=params.lifetime_charges)
     @variable(model, maximum_capacities[1:N], lower_bound=0.0, upper_bound=params.max_storage_volume)
     @variable(model, t[1:N], lower_bound=-params.max_storage_volume, upper_bound=params.max_storage_volume) # dummy variable
@@ -68,43 +74,73 @@ function optimise_battery_charge(prices, params::BatteryParams, del_t=1800)
     # Initialisation
     fix(energies[1], 0.0; force=true) # Assume that the battery is not charged at all 
     fix(cycles[1], 0.0; force=true)
-    fix(maximum_capacities[1], params.max_storage_volume; force=true) # For there is no change in the max capacities 
+    for i in 1:N
+        fix(maximum_capacities[i], params.max_storage_volume; force=true) # For there is no change in the max capacities 
+    end 
     # Inequality constraints 
-    @constraint(model, energy_min_max[i=1:N], energies[i] <= maximum_capacities[i])
-    @constraint(model, energy_out_con[i=1:N], energy_out1[i] <= (energies[i])) 
-    @constraint(model, energy_in_con[i=1:N], energy_in1[i] <= maximum_capacities[i] - energies[i] )
-    @constraint(model, abs_pos_con[i=1:N], energy_in1[i] - energy_out1[i] <= t[i])
-    @constraint(model, abs_neg_con[i=1:N], -(energy_in1[i] - energy_out1[i]) <= t[i])
+    @constraint(model, energy_min_max, energies <= maximum_capacities)
+    @constraint(model, energy_out_con, energy_out1 + energy_out2 <= (energies)) 
+    @constraint(model, energy_in_con, energy_in1 + energy_in1 <= maximum_capacities - energies )
+    @constraint(model, abs_pos_con, energy_in1 + energy_in2 - energy_out1 - energy_out2 <= t)
+    @constraint(model, abs_neg_con, -(energy_in1 + energy_in2 - energy_out1 - energy_out2) <= t)
 
     # Equality constraints 
     num_of_hrs = del_t / 3600
-    const_nat_log_deg_rate = log(params.degradation_rate)
-    @constraint(model, power_con, powers == (energy_in1 - energy_out1)./num_of_hrs)
-    @constraint(model, energy_con[i=1:N-1], energies[i+1] == energies[i]+(energy_in1[i] - energy_out1[i])*params.charging_efficiency) # For now just assume charging and discharging gives the same 
+    @constraint(model, power_con, powers == (energy_in1 + energy_in2 - energy_out1 - energy_out2)./num_of_hrs)
+    @constraint(model, energy_con[i=1:N-1], energies[i+1] == energies[i]+(energy_in1[i] + energy_in2[i] - energy_out1[i] - energy_out2[i])*params.charging_efficiency) # For now just assume charging and discharging gives the same 
     @constraint(model, cycles_con[i=1:N-1], (cycles[i+1] - cycles[i])*params.max_storage_volume == t[i]) 
-    @constraint(model, maximum_cap_con[i=2:N], maximum_capacities[i] == maximum_capacities[i-1] - maximum_capacities[i-1]*(1 + const_nat_log_deg_rate*(cycles[i] - cycles[i-1])))
 
     # Reduce the cost as much as possible
-    @objective(model, Min, sum((energy_in1[i] - energy_out1[i])*prices[i] for i in 1:N))
+    @objective(model, Min, transpose(prices[:,1])*(energy_in1 - energy_out1) + transpose(prices[:,2])*(energy_in2 - energy_out2))
 
     optimize!(model)
     @assert is_solved_and_feasible(model)
     solution_summary(model)
 
-    if overflow > 0.0
-        # Add overflow zeros to the end
-
-    end
-    energies_mat = value.(energies)
-    energies_in1_mat = value.(energy_in1)
-    energies_out1_mat = value.(energy_out1)
+    
+    energies_arr = value.(energies)
+    energies_in1_arr = value.(energy_in1)
+    energies_out1_arr = value.(energy_out1)
+    energies_in2_arr = value.(energy_in2)
+    energies_out2_arr = value.(energy_out2)
+    cycles_arr = value.(cycles)
+    max_cap_arr = value.(maximum_capacities)
+    powers_arr = value.(powers)
     for i in 1:(N-1)
-        lhs_val = energies_mat[i+1]
-        rhs_val = energies_mat[i] + (energies_in1_mat[i] - energies_out1_mat[i])*params.charging_efficiency
+        lhs_val = energies_arr[i+1]
+        rhs_val = energies_arr[i] + (energies_in1_arr[i] + energies_in2_arr[i] - energies_out1_arr[i] - energies_out2_arr[i])*params.charging_efficiency
         @assert  isapprox(lhs_val, rhs_val; atol=1e-5) 
     end
 
-    return value.(energy_in1), value.(energy_out1), value.(energies), value.(cycles), value.(maximum_capacities), value.(powers)
+    if overflow > 0
+        # Add overflow zeros to the end
+        num_of_zeros = overflow 
+        last_val_energy = energies_arr[-1]
+        last_val_energy_in1 = 0.0
+        last_val_energy_in2 = 0.0
+        last_val_energy_out1 = 0.0
+        last_val_energy_out2 = 0.0
+        last_val_cycles = cycles_arr[-1]
+        last_val_max_cap = max_cap_arr[-1]
+        last_val_powers = 0.0
+        while num_of_zeros > 0 
+            push!(energies_arr, last_val_energy)
+            push!(energies_in1_arr, last_val_energy_in1)
+            push!(energies_in2_arr, last_val_energy_in2)
+            push!(energies_out1_arr, last_val_energy_out1)
+            push!(energies_out2_arr, last_val_energy_out2)
+            push!(cycles_arr, last_val_cycles)
+            push!(max_cap_arr, last_val_max_cap)
+            push!(powers_arr, last_val_powers)
+            num_of_zeros -= 1
+        end
+    end
+
+    # Concaternate the energies in and out
+    energies_in_mat = hcat(energies_in1_arr, energies_in2_arr)
+    energies_out_mat = hcat(energies_out1_arr, energies_out2_arr)
+
+    return energies_in_mat, energies_out_mat, energies_arr, cycles_arr, max_cap_arr, powers_arr
 end
 
 end
--- plot_funcs.jl
+++ plot_funcs.jl
@@ -4,14 +4,31 @@ using CSV, DataFrames, Infiltrator, YAML, Plots, Dates
 using ..BatteryModel
 """
 Calculate the total yearly profits of the battery
+
+- prices - N x 2 matrix representing the prices from market 1 (col 1) and market 2 (col 2) in gbp / MWh
+- energies_in - N x 2 matrix representing the energy charging the battery from market 1 (col 1) and market 2 (col 2)
+- energies_out - N x 2 matrix representing the energy discharging from the battery to market 1 (col 1) and market 2 (col 2)
+- battery_params - primarily need the capex and opex 
+- years - operational years of the battery
 """
 function calculate_yearly_profits(prices, energies_in, energies_out, battery_params::BatteryParams, years)
-    net_revenue = - sum(prices.*(energies_in.-energies_out))
+    net_revenue = - transpose(prices[:,1])*(energies_in[:,1] - energies_out[:,1]) + transpose(prices[:,2])*(energies_in[:,2] - energies_out[:,2])
     return (net_revenue - battery_params.opex*years - battery_params.capex) / years, net_revenue
 end
 
 """
 plot_battery_performance(prices, charges, del_t=1800)
+
+Inputs: 
+- prices - N x 2 matrix representing the prices from market 1 (col 1) and market 2 (col 2) in gbp / MWh
+- energies_in - N x 2 matrix representing the energy charging the battery from market 1 (col 1) and market 2 (col 2)
+- energies_out - N x 2 matrix representing the energy discharging from the battery to market 1 (col 1) and market 2 (col 2)
+- energies - N x 1 vector representing the battery energy
+- powers - N x 1 vector representing the battery power
+- cycles - N x 1 vector representing the battery cycles
+- maximum_capacities - N x 1 vector representing the max battery capacity
+- N is the number of timesteps
+
 - This plots 6 graphs 
 - Plot of the prices of each of the markets
 - Plot of the energies in of each of the markets
@@ -22,15 +39,28 @@ plot_battery_performance(prices, charges, del_t=1800)
 - Plot of the total costs of each of the markets 
 """
 function plot_battery_performance(prices, energies_in, energies_out, energies, powers, cycles, maximum_capacities, del_t=1800, save_folder="plots/")
-    N = length(prices) # Will have to change later
-    revenue =  - prices .* (energies_in .- energies_out)
+    N = size(prices)[1] # Will have to change later
+    revenues1 = - prices[:,1].*(energies_in[:,1] - energies_out[:,1])
+    revenues2 = - prices[:,2].*(energies_in[:,2] - energies_out[:,2])
+    revenues = revenues1 + revenues2
     time = del_t .* (1:N) ./ 86400 # in days
 
-    p_prices = plot(time, prices, xlabel="Time (days)", ylabel="Prices (£)", legend=false)
+    p_prices = plot(time, prices[:,1], xlabel="Time (days)", ylabel="Prices (£)", label="Market 1 Price")
+    p_prices = plot!(time, prices[:,2], label="Market 2 Price")
     savefig(p_prices, save_folder*"prices_fig.png")
 
-    p_energies_in = plot(time, energies_in, xlabel="Time (days)", ylabel="Energy into Battery (MWh)", legend=false)
+    p_energies_in = plot(time, energies_in[:,1], xlabel="Time (days)", ylabel="Energy into Battery (MWh)", label="Market 1 intake")
+    p_energies_in = plot!(time, energies_in[:,2], label="Market 2 intake")
     savefig(p_energies_in, save_folder*"energies_in_fig.png")
+
+    p_energies_out = plot(time, energies_out[:,1], xlabel="Time (days)", ylabel="Energy out (MWh)", label="Market 1 output")
+    p_energies_out = plot!(time, energies_out[:,2], label="Market 2 output")
+    savefig(p_energies_out, save_folder*"energies_out_fig.png")
+
+    p_revenues = plot(time, revenues, xlabel="Time (date)", ylabel="Revenue (GBP)", label="Total revenues")
+    p_revenues = plot!(time, revenues1, xlabel="Time (date)", ylabel="Revenue (GBP)", label="Market 1 revenues")
+    p_revenues = plot!(time, revenues2, xlabel="Time (date)", ylabel="Revenue (GBP)", label="Market 2 revenues")
+    savefig(p_revenues, save_folder*"revenues_fig.png")
 
     p_energy = plot(time, energies, xlabel="Time (days)", ylabel="Battery Energy (MWh)", legend=false)
     savefig(p_energy, save_folder*"battery_energy_fig.png")
@@ -44,16 +74,22 @@ function plot_battery_performance(prices, energies_in, energies_out, energies, p
     p_max_cap = plot(time, powers, xlabel="Time (days)", ylabel="Power in (MW)", legend=false)
     savefig(p_max_cap, save_folder*"power_fig.png")
 
-    p_energies_out = plot(time, energies_out, xlabel="Time (days)", ylabel="Energy out (MWh)", legend=false)
-    savefig(p_energies_out, save_folder*"energies_out_fig.png")
-
-    p_revenues = plot(time, revenue, xlabel="Time (date)", ylabel="Revenue (GBP)", legend=false)
-    savefig(p_revenues, save_folder*"revenues_fig.png")
 end
-
+"""
+Inputs: 
+- prices - N x 2 matrix representing the prices from market 1 (col 1) and market 2 (col 2) in gbp / MWh
+- energies_in - N x 2 matrix representing the energy charging the battery from market 1 (col 1) and market 2 (col 2)
+- energies_out - N x 2 matrix representing the energy discharging from the battery to market 1 (col 1) and market 2 (col 2)
+- energies - N x 1 vector representing the battery energy
+- powers - N x 1 vector representing the battery power
+- cycles - N x 1 vector representing the battery cycles
+- maximum_capacities - N x 1 vector representing the max battery capacity
+- start_time - start timestep of the input data
+- N is the number of timesteps
+"""
 function write_battery_performance(prices, energies_in, energies_out, energies, cycles, maximum_capacities, start_time, battery_params::BatteryParams, del_t=1800)
     # Write the total revenue to a text file
-    N = length(prices)
+    N = size(prices)[1]
     years = N * del_t/ 31536000
     total_profits, net_revenue = calculate_yearly_profits(prices, energies_in, energies_out, battery_params, years)
     file = open("data/output_data/total_profits.txt", "w")
@@ -64,7 +100,7 @@ function write_battery_performance(prices, energies_in, energies_out, energies, 
     # Write the data to a CSV file
     del_timestep = Dates.Second(del_t)
     time_steps = [start_time + i*del_timestep for i in 0:(N-1)]
-    output_df = DataFrame(Time=time_steps, Energy_in=energies_in, Energy_out=energies_out, total_energies=energies, battery_cycles=cycles, maximum_capacities=maximum_capacities)
+    output_df = DataFrame(Time=time_steps, Energy_in1=energies_in[:,1], Energy_in2=energies_in[:,2], Energy_out1=energies_out[:,1], Energies_out2=energies_out[:,2], total_energies=energies, battery_cycles=cycles, maximum_capacities=maximum_capacities)
     CSV.write("data/output_data/battery_output_data.cvs", output_df)
 end
 
